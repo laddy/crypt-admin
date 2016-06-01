26277affa521c7477c2059afc2104627440e95a1
Yuya Tanaka <Yuya Tanaka>
2016-06-02 01:45:48 +0900

    Add: Mongo Insert And Search

--- app.js
+++ app.js
@@ -15,38 +15,14 @@ app.set('view engine', 'ect');
 
 console.log(crypto.createHash('sha256').update('hogehoge'+crypt_salt).digest('hex'));
 
-mongodb.MongoClient.connect("mongodb://ds030829.mlab.com:30829/crypt-admin", function(err, database) {
-    users = database.collection("users");
+var service;
+var users;
+mongo.MongoClient.connect("mongodb://laddy:laddymongo@ds030829.mlab.com:30829/crypt-admin", function(err, database) {
+    users   = database.collection("users");
+    service = database.collection("service");
+
 });
 
-users.find().toArray(function(err, items) {
-    console.log(items);
-});
-/*
-var Collection;
-db.open(function() {
-    db.authenticate('laddy', 'laddymongo', function(err, result) {
-        });
-    /*
-    db.collection('teacherCollection', function(err, collection) {
-        doc = {
-            "firstname" : "Taro",
-            "familyname" : "Yamada",
-            "age" : 42,
-            "work" : ["professor", "writer", "TV Caster"]
-        };
-        collection.insert(doc, function() {
-            console.log("insert success");
-        });
-
-    });
-    db.collection('service').find();
-    db.cursor.each(function(err, doc) {
-        console.log(doc);
-    });
-    db.close();
-});
-*/
 function crypto_convert(text)
 {
     var cipher  = crypto.createCipher('aes-256-cbc', 'password');
@@ -77,12 +53,9 @@ app.post('/', function(req, res) {
 
 // ログイン後画面表示
 app.get('/acclist', function(req, res) {
-    res.render('acclist', {
-        service: [
-            { service: 'Google', url: 'https://google.co.jp/', id: '12345', pass: 'xxxxxxxx'},
-            { service: 'Facebook', url: 'https://facebook.com/', id: '12345', pass: 'yyyyy'},
-            { service: 'Twitter', url: 'https://twitter.com/', id: '12345', pass: 'zzzzz' }
-        ]
+    service.find().toArray(function(err, items) {
+        console.log(items);
+        res.render('acclist', items);
     });
 });
 
@@ -95,9 +68,17 @@ app.get('/admin-user', function(req, res) {
 // サービス登録
 app.get('/admin-service', function(req, res) {
     res.render('admin-service');
+    service.find().toArray(function(err, items) {
+        console.log(items);
+        res.render('admin-service', items);
+    });
 });
-
-
+app.post('/admin-service', function(req, res) {
+    console.log(res.body);
+    // Save Mongo
+    service.insert(req.body);
+    
+})
 
 app.get('/test', function(req, res){
 
