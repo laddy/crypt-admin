3e3a3a0c9084881e9ac4562df74f11785634834e
laddy <laddy.com@gmail.com>
2016-06-15 13:44:49 +0900

    ファイル整理 DBファイルの外部化 Adminユーザのみ利用する想定に使用変更

--- app.js
+++ app.js
@@ -6,15 +6,14 @@ var ECT          = require('ect');
 var session      = require('express-session');
 var mongoStore   = require('connect-mongo')(session);
 var cookieParser = require('cookie-parser')
-
-var bodyParser = require('body-parser');
+var bodyParser   = require('body-parser');
 
 
 var app     = express();
 var server  = app.listen(3000);
 
 var crypt_salt = 'GYw-HB35AHsTVmKVyJ7Ur6JLhaQHPiWS';
-
+var config = require('./config');
 
 function crypto_convert(text)
 {
@@ -37,6 +36,22 @@ function decrypto_convert(text)
 
 }
 
+var service;
+var users;
+
+mongo.MongoClient.connect("mongodb://" +config.dbuser+
+    ":" +config.dbpass+ "@" +config.dbhost+ ":" +config.dbport+ "/" +config.dbdb,
+    function(err, database) {
+        users   = database.collection("users");
+        service = database.collection("service");
+        console.log('in connect');
+    }
+);
+
+/*
+ * Middle Ware
+ */
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(session({         // cookieに書き込むsessionの仕様を定める
     secret: 'secret',               // 符号化。改ざんを防ぐ
@@ -51,76 +66,44 @@ app.use(session({         // cookieに書き込むsessionの仕様を定める
     }
 }));
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.engine('ect', ECT({ watch: true, root: __dirname + '/views', ext: '.ect' }).render);
-app.set('view engine', 'ect');
-
 console.log(crypto.createHash('sha256').update('admin'+crypt_salt).digest('hex'));
 // console.log(decrypto_convert(crypto_convert('admin')));
 
-var service;
-var users;
+app.engine('ect', ECT({ watch: true, root: __dirname + '/views', ext: '.ect' }).render);
+app.set('view engine', 'ect');
 
-mongo.MongoClient.connect("mongodb://laddy:laddymongo@ds030829.mlab.com:30829/crypt-admin", function(err, database) {
-    users   = database.collection("users");
-    service = database.collection("service");
-    console.log('in connect');
-});
 
 
 
 // ログイン画面表示
 app.get('/', function(req, res) {
-    console.log(req.session.id);
-    res.render('index', {title1 : 'express test title1'});
-});
-app.post('/', function(req, res) {
-    users.findOne({
-            login : req.body.userid,
-            password : crypto.createHash('sha256').update(req.body.password + crypt_salt).digest('hex')
-        },
-        function (err, authuser) {
-            if ( !authuser ) {
-                res.render('index');
-            }
-            else if ( 1 < Object.keys(authuser).length ) {
-                req.session.user = {
-                    id   : authuser.login,
-                    name : authuser.name,
-                    pwd  : authuser.password,
-                };
-                console.log('in login');
-
-                // Admin go all service page
-                if ( 'admin' == authuser.login ) {
-                    res.redirect('admin-service');
-                }
-                else {
-                    res.redirect('acclist');
-                }
-            }
-        }
-    );
-});
-
-
-// ログイン後画面表示
-app.get('/acclist', function(req, res) {
-    service.find().toArray(function(err, items) {
-        console.log(items);
-        res.render('acclist', items);
-    });
-});
-
-
-// ユーザ登録
-app.get('/admin-user', function(req, res) {
-    if ( 'admin' == req.session.id ) {
-        res.render('admin-user');
+    console.log('Get /');
+    // Has Session Admin
+    if ( undefined != req.session.user && 'admin' == req.session.user.id  ) {
+        console.log('Redirect /admin-service');
+        res.redirect('admin-service');
     }
     else {
-        res.render('acclist');
+        console.log('Redirect /index');
+        res.render('index');
     }
+});
+// Post Login Data
+app.post('/', function(req, res) {
+    console.log('Post /');
+    users.findOne({
+        login    : req.body.userid,
+        password : crypto.createHash('sha256').update(req.body.password + crypt_salt).digest('hex')
+    },
+    function (err, authuser) {
+        if ( null != authuser && 1 < Object.keys(authuser).length ) {
+            req.session.user = {
+                id   : authuser.login,
+                name : authuser.name,
+            };
+        }
+        res.render('index', {err : 'Error: UserID or Password'});
+    });
 });
 
 
@@ -129,8 +112,8 @@ app.get('/admin-user', function(req, res) {
  */
 // 全サービス表示
 app.get('/admin-service/', function(req, res) {
-    if ( 'admin' != req.session.id ) {
-        res.render('acclist');
+    if ( 'admin' != req.session.user.id ) {
+        res.redirect('/');
     }
 
     service.find().toArray(function(err, items) {
@@ -142,7 +125,7 @@ app.get('/admin-service/', function(req, res) {
 });
 // Save Service Data
 app.post('/admin-service', function(req, res) {
-    if ( 'admin' != req.session.id ) {
+    if ( 'admin' != req.session.user.id ) {
         res.render('acclist');
     }
 
@@ -166,8 +149,8 @@ app.post('/admin-service', function(req, res) {
 
 // Service Edit
 app.get("/admin-edit/:_id", function(req, res) {
-    if ( 'admin' != req.session.id ) {
-        res.render('acclist');
+    if ( 'admin' == req.session.user.id ) {
+        res.render('admin-edit');
     }
     if ( 'new' !== req.params._id )
     {
@@ -176,15 +159,9 @@ app.get("/admin-edit/:_id", function(req, res) {
             res.render('admin-edit', item);
         });
     }
-    else
-    {
-        res.render('admin-edit');
+    else {
+        res.redirect('acclist');
     }
-});
-
-app.get('/test', function(req, res){
-
-
 });
 
 
---
+++ config.js
@@ -0,0 +1,17 @@
+exports.dbhost = "ds030829.mlab.com";
+exports.dbport = "30829";
+exports.dbdb   = "crypt-admin";
+exports.dbuser = "laddy";
+exports.dbpass = "laddymongo";
+
+/*
+{
+    "dbhost" : "ds030829.mlab.com",
+    "dbport" : "30829",
+    "dbdb"   : "crypt-admin",
+    "dbuser" : "laddy",
+    "dbpass" : "laddymongo",
+}
+*/
+
+exports.abc = 'hoge';
